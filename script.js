2765e769a878506e835860e2267c13f5667f8459
Luigi <luigibenedettomt>
2019-04-25 01:03:13 -0300

    Changes in the main structure

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 const fs = require ('fs');
-'use strict';
-console.log("Pense em um personagem ou pessoa");
-var input_primario = process.stdin;
-input_primario.setEncoding('utf-8');
-
-input_primario.on ('data', function(data){
-    if(data === 'exit\n'){
-        process.exit();
-    }
-});
 
 var questions = [
+    
     {
-    pergunta1: "Seu personagem é homem?\n(a) Sim\n\
-                (b) Não\n(c) Não sei",
-    resposta1: data
+        pergunta1: "Seu personagem é homem?\n(a) Sim\n\
+        (b) Não\n(c) Não sei",
+        resposta1: ""
     },
     {
-    pergunta2: "Seu personagem tem mais de 20 anos?\n(a) Sim\n\
-                (b) Não\n(c) Não sei",
-    resposta2: data 
+        pergunta2: "Seu personagem tem mais de 20 anos?\n(a) Sim\n\
+        (b) Não\n(c) Não sei",
+        resposta2: data 
+    },
+    {
+        pergunta3: "Seu personagem é um desenho animado?\n(a) Sim\n\
+        (b) Não\n(c) Não sei",
+        resposta3: data
     }
 ]
+
+//var firstItem = "Pense em um personagem ou pessoa"
+
+for(var i = 0; i < questions.length; i++){
+    var response =  window.prompt(questions[i].prompt);
+}
